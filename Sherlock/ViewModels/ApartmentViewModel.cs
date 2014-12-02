8e6e9c2a48ded8b1d8a6a42860055e074af1e8be
Tomer Buzaglo <Tomer.bu@gmail.com>
2014-12-02 16:51:56 +0200

    Revert "Deleted a file"
    This reverts commit 8f2cede26f14cfff5558701c7a2faf15d2c52f81.

---
+++ Sherlock/ViewModels/ApartmentViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Sherlock.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sherlock.ViewModels
+{
+    public class ApartmentViewModel
+    {
+        public Apartment Apartment { get; set; }
+        public Address Address { get; set; }
+    }
+}
